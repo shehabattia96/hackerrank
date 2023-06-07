87a1f734c63328e0bf9da88aa5ff69d5c491d2d9
Shehab Attia <shehabattia96@gmail.com>
2023-06-07 18:54:21 -0400

    Added a C++ template.

---
+++ template_c++.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+
+namespace Utils
+{
+
+}
+
+using namespace std;
+using namespace Utils;
+
+class Tests
+{
+public:
+    void runAllTests()
+    {
+    }
+};
+
+class Solution
+{
+public:
+};
